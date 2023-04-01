d762fae9684dff009c00c7eb01a94e8c740f9b5f
Shankha Ghosh <shankhagh.31@gmail.com>
2023-04-02 02:42:16 +0530

    routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./index.css";
 import Home from "./Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import MovieSummary from "./MovieSummary";
 import BookingPage from "./BookingPage";
 
 function App() {
     return (
         <>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="movie/:id" element={<MovieSummary />} />
                     <Route path="/booking/:id" element={<BookingPage />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
 
         </>
 
